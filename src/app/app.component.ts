9115ce29f2db7c203b09b9d66c8b2ff5f21e1f86
Denys Vladymyrov <link_amd@yahoo.com>
2017-08-10 16:37:32 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  shoppingList = [
+    {name: "Молоко", done: false},
+    {name: "Творог", done: false},
+    {name: "Хлеб", done: false},
+    {name: "Зубная паста", done: false},
+    {name: "Картошка", done: false},
+    {name: "Помидоры", done: false},
+    {name: "Стиральный порошок", done: false},
+    {name: "Гель для душа", done: false}
+  ];
+
+
+  addProduct(product){
+   if(product){
+    this.shoppingList.push({name: product.value, done: false});
+    product.value = "";
+   }
+  }
+
+  productClick(item){
+    item.done = !item.done;
+  }
+
+ 
+
 }
